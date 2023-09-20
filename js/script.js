85fc607b4d59e8ba4a03c10a8d77908106192d3b
ssggtt <ssggtt@mail.ru>
2023-09-20 14:28:42 +0300

    add slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-	console.log('proverka script.js');
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const bannerSwiper = new Swiper(".banner-swiper", {
+    speed: 1000,
+    spaceBetween: 0,
+    navigation: {
+      nextEl: ".banner-swiper .swiper-button-next",
+      prevEl: ".banner-swiper .swiper-button-prev",
+    },
+  });
+});
